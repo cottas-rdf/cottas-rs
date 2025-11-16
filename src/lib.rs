649bc6a5e6cec636114e8307c00549b19e6b0757
savacano28 <stephanya.casanova@filigran.io>
2025-11-16 23:06:54 +0100

    chore: initial project cottas-rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,13 @@
-pub mod duckdb;
-pub mod export;
-pub mod parser;
-pub mod utils;
+pub fn cottas_rs() -> &'static str {
+    "Hello from cottas-rs!"
+}
 
-use std::error::Error;
-pub use duckdb::{connection_in_memory, load_into_duckdb};
-pub use export::{export_to_cottas};
-pub use parser::parse_rdf_file;
-pub use utils::build_order_by;
-pub use utils::extract_format;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-pub fn rdf2cottas(rdf_file_path: &str, cottas_file_path: &str, index: &str) -> Result<(), Box<dyn Error>>{
-    let quads = parse_rdf_file(rdf_file_path)?;
-    let quad_mode = quads.iter().any(|q| q.3.is_some());
-    let conn = load_into_duckdb(&quads);
-    export_to_cottas(&conn, index, cottas_file_path, quad_mode);
-    Ok(())
+    #[test]
+    fn test_cottas_rs() {
+        assert_eq!(cottas_rs(), "Hello from cottas-rs!");
+    }
 }
